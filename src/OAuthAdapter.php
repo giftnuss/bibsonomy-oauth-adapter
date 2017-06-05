dd4dce28ad2c6dca2975d7263e7cad06628af2e6
Sebastian B. Knapp <news@young-workers.de>
2017-06-05 21:38:08 +0200

    update experiment, first commit

--- src/OAuthAdapter.php
+++ src/OAuthAdapter.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
     Copyright (C) 2015 - Sebastian Böttger <boettger@cs.uni-kassel.de>
 
     This program is free software: you can redistribute it and/or modify
@@ -17,10 +17,13 @@
 */
 
 namespace AcademicPuma\OAuth;
+
 use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
 
 /**
- * 
+ *
  *
  * @author Sebastian Böttger <boettger@cs.uni-kassel.de>
  */
@@ -28,7 +31,7 @@ class OAuthAdapter {
 
 
     const REQUEST_TOKEN_URL = 'oauth/requestToken';
-    
+
     const ACCESS_TOKEN_URL = 'oauth/accessToken';
 
     /**
@@ -36,82 +39,90 @@ class OAuthAdapter {
      * @var string
      */
     public $authorizeURL = 'oauth/authorize';
-    
-    
+
+
     public static $CLIENT_METHODS = ['get', 'post', 'put', 'delete', 'head', 'options', 'patch'];
-    
+
     /**
      *
      * @var array
      */
     protected $config;
-    
+
     /**
      *
      * @var Token\ConsumerToken
      */
     protected $consumerToken;
-    
+
     /**
      *
-     * @var \GuzzleHttp\Client 
+     * @var \GuzzleHttp\Client
      */
     protected $client;
-    
+
     /**
      *
-     * @var \AcademicPuma\OAuth\Subscriber\BibSonomySubscriber 
+     * @var \AcademicPuma\OAuth\Subscriber\BibSonomySubscriber
      */
     protected $bibsonomySubscriber;
-    
+
     /**
-     * 
+     *
      * @var array
      */
     protected $_parameters;
-    
+
     /**
-     * 
+     *
      * @param array $config <code>['consumerKey' => '','consumerSecret => '', 'callbackUrl' => '', 'baseUrl' => '']</code>
      */
     public function __construct(array $config = []) {
-        
+
         $this->config = $config;
-        
+
         $this->client = new Client([
-            'base_url' => $this->addTrailingSlash($config['baseUrl']),
+            'base_uri' => $this->addTrailingSlash($config['baseUrl']),
             'defaults' => ['auth' => 'oauth']
         ]);
 
         if (!empty($config['authUrl'])) $this->authorizeURL = $this->removeTrailingAndLeadingSlash($config['authUrl']);
-        
+
         $this->consumerToken = new Token\ConsumerToken($config['consumerKey'], $config['consumerSecret']);
-        
+
         $this->bibsonomySubscriber = new Subscriber\BibSonomySubscriber($this->consumerToken);
     }
-    
+
     /**
-     * 
+     *
      * @return \AcademicPuma\OAuth\Token\ConsumerToken
      */
     public function getConsumerToken() {
-        
+
         return $this->consumerToken;
     }
-    
+
     /**
-     * 
+     *
      * @return \AcademicPuma\OAuth\Token\RequestToken
      */
-    public function getRequestToken() {
-        
-        $this->client->getEmitter()->attach($this->bibsonomySubscriber->getRequestTokenSubscriber());
-        
-        $res = $this->client->post(
+    public function getRequestToken()
+    {
+        $stack = HandlerStack::create();
+        $middleware = $this->bibsonomySubscriber->getRequestTokenSubscriber();
+        $stack->push($middleware);
+
+        $config = $this->config;
+        $client = new Client([
+            'base_uri' => $this->addTrailingSlash($config['baseUrl']),
+            'handler' => $stack
+        ]);
+
+        $res = $client->post(
                 self::REQUEST_TOKEN_URL,
-                ['body' => ['oauth_callback' => $this->config['callbackUrl']]]
+                ['form_params' => ['oauth_callback' => $this->config['callbackUrl']]]
         );
-        
+
         return new Token\RequestToken($res);
     }
 
@@ -122,30 +133,35 @@ class OAuthAdapter {
      * @return Token\AccessToken
      */
     public function getAccessToken(Token\RequestToken $requestToken) {
-        
+
         $authToken = filter_input(INPUT_GET, 'oauth_token', FILTER_SANITIZE_STRING);
         $userId    = filter_input(INPUT_GET, 'user_id', FILTER_SANITIZE_STRING);
-        
+
         if($requestToken->getOauthToken() === $authToken) {
+            $stack = HandlerStack::create();
             $subscriber = $this->bibsonomySubscriber->getAccessTokenSubscriber($requestToken);
-            $this->client->getEmitter()->attach($subscriber);
-            $res = $this->client->post(self::ACCESS_TOKEN_URL, ['body' => ['user_id' => $userId]]);
-            
+            $stack->push($subscriber);
+
+            $client = new Client([
+                'base_uri' => $this->addTrailingSlash($config['baseUrl']),
+                'handler' => $stack
+            ]);
+            $res = $client->post(self::ACCESS_TOKEN_URL, ['form_params' => ['user_id' => $userId]]);
+
             $accessToken = new Token\AccessToken($res);
-            $this->client->getEmitter()->detach($subscriber);
-            
+
             return $accessToken;
         }
-        
+
         throw new \InvalidArgumentException("Error: The oauth_token from callback is not the same as the request_token");
     }
-    
+
     /**
-     * 
+     *
      * @param \AcademicPuma\OAuth\Token\RequestToken $requestToken
      */
     public function redirect(Token\RequestToken $requestToken) {
-            
+
         $params_ = $this->assembleRedirectParams($requestToken);
         $encodedParams = array();
         foreach ($params_ as $key => $value) {
@@ -154,22 +170,22 @@ class OAuthAdapter {
                              . self::urlEncode($value);
         }
         $params = implode('&', $encodedParams);
-        
+
         header('Location: ' . $this->buildAuthorizeUrl($params));
     }
-    
+
     private function buildAuthorizeUrl($params) {
-        
+
         return $this->config['baseUrl'] . $this->authorizeURL . '?' . $params;
     }
-    
+
     private function assembleRedirectParams(Token\RequestToken $requestToken) {
         $params = array(
             'oauth_token' => $requestToken->getOauthToken()
         );
 
         $params['oauth_callback'] = $this->config['callbackUrl'];
-        
+
         if (!empty($this->_parameters)) {
             $params = array_merge($params, $this->_parameters);
         }
@@ -185,35 +201,39 @@ class OAuthAdapter {
      * @return Client
      */
     public function prepareClientForOAuthRequests(Token\AccessToken $accessToken) {
+        $stack = HandlerStack::create();
+        $subscriber = $this->bibsonomySubscriber->getOAuthSubscriber($accessToken);
+        $stack->push($subscriber);
+
         $this->client = new Client([
-            'base_url' => $this->config['baseUrl'] . 'api', 
-            'defaults' => ['auth' => 'oauth']
+            'base_uri' => $this->config['baseUrl'] . 'api',
+            'defaults' => ['auth' => 'oauth'],
+            'handler' => $stack
         ]);
-        
-        $this->client->getEmitter()->attach($this->bibsonomySubscriber->getOAuthSubscriber($accessToken));
+
         return $this->client;
     }
-    
+
     public function __call($method, array $args = []) {
-        
+
         if (!in_array($method, self::$CLIENT_METHODS)) {
             throw new \BadMethodCallException("Method $method does not exist on " . __CLASS__);
         }
-        
+
         $url = isset($args[0]) ? $args[0] : null;
         $options = isset($args[1]) ? $args[1] : [];
-        
+
         // trigger request
         return $this->client->$method($url, $options);
     }
-    
+
     /**
-     * 
+     *
      * @param string $value
      * @return string
      */
     public static function urlEncode($value){
-        
+
         $encoded_ = rawurlencode($value);
         $encoded = str_replace('%7E', '~', $encoded_);
         return $encoded;
